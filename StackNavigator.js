f57e4c8f809ddb8fcc29f154fc1267e77aa68f6a
Muhammad-Sameed-Ansari <52503336+Muhammad-Sameed-Ansari@users.noreply.github.com>
2022-10-05 21:03:12 +0500

    Phone Auth Screen in progress

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,11 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import OTPScreen from './screens/OTPScreen';
+import PhoneNumScreen from './screens/PhoneNumScreen';
+
+const Stack = createStackNavigator();
 
 const StackNavigator = () => {
     return (
-        <View>
-            <Text>StackNavigator</Text>
-        </View>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Phone" component={PhoneNumScreen} />
+            <Stack.Screen name="OTP" component={OTPScreen} />
+        </Stack.Navigator>
     )
 }
 
---
+++ screens/PhoneNumScreen.js
@@ -0,0 +1,15 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Icon from 'react-native-vector-icons/Feather'
+
+const PhoneNumScreen = () => {
+    return (
+        <SafeAreaView>
+            <Icon name="chevron-left" size={24} />
+        </SafeAreaView>
+    )
+}
+
+export default PhoneNumScreen
+
+const styles = StyleSheet.create({})
